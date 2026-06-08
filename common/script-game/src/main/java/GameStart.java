30450ad4865a21604d286647efd01d6050b6e4f8
yeluozhishi <x1416681015@qq.com>
2026-06-08 08:01:18 +0800

    开始游戏服务端

--- common/script-game/src/main/java/GameStart.java
+++ common/script-game/src/main/java/GameStart.java
@@ -1,8 +1,4 @@
 import com.whk.GameApplication;
-import script.ScannerClassException;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 public class GameStart {
     public static void main(String[] args) {
