703c6063de640cdb981d5af6640143efe88b2c44
yeluozhishi <x1416681015@qq.com>
2026-06-14 23:30:54 +0800

    更新

--- common/protobuf/src/main/java/com/whk/BuildJavaProto.ps1
+++ common/protobuf/src/main/java/com/whk/BuildJavaProto.ps1
@@ -3,7 +3,8 @@ echo $origin_path
 $path = $PWD.Path.ToString().replace("com\whk", "")
 echo $path
 
-foreach($filepath in (Get-ChildItem $origin_path *.proto -Recurse)){
-echo $filepath building...
-.\protoc.exe --proto_path=$origin_path --java_out=$path $filepath.ToString()
+foreach ($filepath in (Get-ChildItem $origin_path *.proto -Recurse))
+{
+    echo $filepath building...
+    .\protoc.exe --proto_path=$origin_path --java_out=$path $filepath.ToString()
 }
